3edaa95652b14be1883588e17449515079e5e534
BroGames <hunempire11@gmail.com>
2023-12-13 13:11:26 +0100

    hello main

--- sortings.py
+++ sortings.py
@@ -1,19 +1,22 @@
 def maximum_value(array):
-    max = array[0]
+    Max = array[0]
     for num in array:
-        if num > max:
-            max = num
-    return max
+        if num > Max:
+            Max = num
+    return Max
 
-def counting_sort(array,maxv):
+
+def counting_sort(array, maxv):
     sorted = [0 for i in range(len(array))]
-    count = [0 for r in range(maxv+2)]
-    
+    count = [0 for r in range(maxv + 2)]
+
     for num in array:
         count[num] += 1
-    for i in range(maxv+1):
-        count[i] += count[i-1]
+    for i in range(maxv + 1):
+        count[i] += count[i - 1]
     for num in array:
-        sorted[count[num]-1] = num
+        sorted[count[num] - 1] = num
         count[num] -= 1
     return sorted
+
+# hello main
